c10f8f5ae9ebd1ae644c9f60fc9e00cb4e871bae
Douglas Fernandes <douglas@sti.ufpb.br>
2023-10-25 08:02:06 -0300

    Correção na preparação do ambiente

--- dataverse.sh
+++ dataverse.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-# [ -z ${DIR_PROJECT} ] && echo -e 'Informe o diretório do projeto. Ex:\n  export DIR_PROJECT="${HOME}/dev/evor"' && exit 1
+[ -z ${DIR_PROJECT} ] && echo -e 'Informe o diretório do projeto. Ex:\n  export DIR_PROJECT="${HOME}/dev/evor"' && exit 1
 
-export DIR_PROJECT="${HOME}/douglas500/sti/desenvolvimento/repositorios/dv/dataverse/dataverse"
+#altere para o seu diretório de projeto
+export DIR_PROJECT="${HOME}/douglas500/sti/desenvolvimento/repositorios/dataverse/dataverse"
 
 YML="${DIR_PROJECT}/docker-compose-dev.yml"
-OPTSTRING=":udbxh"
+OPTSTRING=":udbhi"
 BUILD=false
 
 # Docker exemplo
 # https://github.com/docker/awesome-compose/tree/master/official-documentation-samples/django/
+
+# instalando o docker na vm 
+function installApps() {
+  echo "informe a senha do usuário: "
+  sudo apt-get update -y
+  sudo apt-get install ca-certificates curl gnupg lsb-release -y
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+  # docker run hello-world 
+  sudo usermod -aG docker $USER  
+  echo "O Docker está Ok!
+Reinicie a sua VM"
+}
 
 function usage {
   echo "./$(basename $0) [${OPTSTRING}]"
   echo "  -u: up dataverse"
   echo "  -d: down dataverse"
   echo "  -b: executa o build"
-  echo "  -x: remove os volumes no down"
+  echo "  -i: Instala o docker"
   echo "  -h: help"  
 }
 
@@ -32,11 +50,12 @@ while getopts ${OPTSTRING} ARG; do
     d)
       ACTION="down" 
       ;;
-    x)
-      RMVOLUME="rm" 
-      ;;
     b) 
       BUILD=true
+      ;;
+    i) 
+      installApps
+      exit 0
       ;;
     h | *) 
       usage
@@ -47,13 +66,12 @@ done
 
 # se o projeto não existir, baixa:
 if [ ! -d "${DIR_PROJECT}" ]; then
- echo "Projeto não existe. Baixando ..."
+ echo "Projeto dataverse ainda não existe. Baixando ..."
  git clone https://github.com/IQSS/dataverse.git ${DIR_PROJECT}
 fi
 
 source ${DIR_PROJECT}/.env
 
-# remove a imagem
 # docker image rm ${APP_IMAGE} -f
 
 #Se a imagem da aplicação não existir então faça ao build
