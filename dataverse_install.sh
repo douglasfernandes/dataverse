1983b80b7a82af4f362cf516cd92270989d6af40
Douglas Fernandes <douglas@sti.ufpb.br>
2023-11-14 21:18:29 -0300

    correção install

---
+++ dataverse_install.sh
@@ -0,0 +1,201 @@
+#!/bin/bash
+# Instala o dataverse em VM 
+
+SOLR_VERSION="9.3.0"
+
+# DOI obtido em aula
+DIR_DOWNLOAD="/home/ubuntu/download"
+cd /home/ubuntu
+[ ! -f ${DIR_DOWNLOAD}/.env ] && touch ${DIR_DOWNLOAD}/.env && echo 'DOI_USERNAME="user_doi"
+DOI_PASSWORD="password_doi"
+POSTGRES_ADMIN_PASSWORD="postgres"
+DATAVERSE_DB="dataverse"
+DATAVERSE_DB_USER="dataverse"
+DATAVERSE_DB_PASSWORD="dataverse"
+ADMIN_EMAIL="email@webmail.br"
+' > ${DIR_DOWNLOAD}/.env  && echo 'Altere o arquivo .env antes de iniciar a instalação!' && exit 1
+source ${DIR_DOWNLOAD}/.env
+
+apt-get update
+apt-get -y install libc6-i386 libc6-x32 libxi6 libxtst6 unzip wget   
+[ ! -f ${DIR_DOWNLOAD}/jdk-17_linux-x64_bin.deb ] && wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb -P ${DIR_DOWNLOAD}/
+chown _apt /var/lib/update-notifier/package-data-downloads/partial/
+apt-get -y install ${DIR_DOWNLOAD}/jdk-17_linux-x64_bin.deb 
+apt-get -y install --reinstall jdk-17
+
+# postgres
+
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get update
+apt-get -y install postgresql-13
+pg_ctlcluster 13 main start
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*' #libera para todas as conexões/" /etc/postgresql/13/main/postgresql.conf
+#Comenta todas as linhas
+sed -i '/^#/! s/.*/#&/g' /etc/postgresql/13/main/pg_hba.conf
+#Adiciona
+sed -i '$a\
+# Dataverse \
+local   all             postgres                                trust \
+host    all             all             127.0.0.1/32            trust \
+host    all             all             ::1/128                 trust \
+host    all             all             0.0.0.0/0               trust' /etc/postgresql/13/main/pg_hba.conf
+pg_ctlcluster 13 main restart
+#service postgresql restart
+pg_ctlcluster 13 main status
+#service postgresql status
+r=$(/usr/bin/psql -At -h 127.0.0.1 -p 5432 -U postgres -d postgres -c 'SELECT 1')
+[ "$r"=="1" ] && echo "[INFO] Instalação do postgres OK !" || echo "[ERRO] Corriga a instalação do postgres !" 
+
+# criando o usuário e db do dataverse . devem constar no .env
+#sudo -u postgres psql -c "create database $DATAVERSE_DB;"
+#sudo -u postgres psql -c "create user $DATAVERSE_DB_USER with encrypted password '$DATAVERSE_DB_PASSOWRD';"
+#sudo -u postgres psql -c "grant all privileges on database $DATAVERSE_DB to $DATAVERSE_DB_USER;"
+
+# solr
+
+useradd -m solr
+mkdir /usr/local/solr
+[ ! -f ${DIR_DOWNLOAD}/solr-9.3.0.tgz ] && wget https://archive.apache.org/dist/solr/solr/9.3.0/solr-9.3.0.tgz -P ${DIR_DOWNLOAD}/
+tar -xvzf ${DIR_DOWNLOAD}/solr-9.3.0.tgz -C /usr/local/solr
+cp -r /usr/local/solr/solr-9.3.0/server/solr/configsets/_default /usr/local/solr/solr-9.3.0/server/solr/collection1
+chown -R solr:solr /usr/local/solr
+cd /usr/local/solr
+
+sed -i 's;<Set name="requestHeaderSize"><Property name="solr.jetty.request.header.size" default="8192" /></Set>;<Set name="requestHeaderSize"><Property name="solr.jetty.request.header.size" default="102400" /></Set>;' /usr/local/solr/solr-9.3.0/server/etc/jetty.xml
+sed -i '$a#Instalação dataverse' /etc/security/limits.conf
+sed -i '$asolr soft nproc  65000' /etc/security/limits.conf
+sed -i '$asolr hard nproc  65000' /etc/security/limits.conf
+sed -i '$asolr soft nofile 65000' /etc/security/limits.conf
+sed -i '$asolr hard nofile 65000' /etc/security/limits.conf 
+sed -i '$aroot soft nproc  65000' /etc/security/limits.conf 
+sed -i '$aroot hard nproc  65000' /etc/security/limits.conf 
+sed -i '$aroot soft nofile 65000' /etc/security/limits.conf 
+sed -i '$aroot hard nofile 65000' /etc/security/limits.conf
+cd /usr/local/solr/solr-9.3.0
+sudo -u solr bin/solr start
+sudo -u solr bin/solr create_core -c collection1 -d server/solr/collection1/conf/
+
+echo '[Unit]
+Description = Apache Solr
+After = syslog.target network.target remote-fs.target nss-lookup.target
+[Service]
+User = solr
+Type = forking
+WorkingDirectory = /usr/local/solr/solr-9.3.0
+ExecStart = bash /usr/local/solr/solr-9.3.0/bin/solr start
+ExecStop = bash /usr/local/solr/solr-9.3.0/bin/solr stop
+ExecReload= bash /usr/local/solr/solr-9.3.0/bin/solr stop
+LimitNOFILE=65000
+LimitNPROC=65000
+Restart=on-failure
+[Install]
+WantedBy = multi-user.target
+' > /etc/systemd/system/solr.service
+sudo -u solr bin/solr stop
+systemctl daemon-reload
+systemctl restart solr.service
+systemctl enable solr.service
+systemctl status solr.service
+
+# Dataverse
+
+apt-get -y install jq imagemagick curl libssl-dev libcurl4-openssl-dev
+useradd -m dataverse
+[ ! -f ${DIR_DOWNLOAD}/dvinstall.zip ] && wget https://github.com/IQSS/dataverse/releases/download/v6.0/dvinstall.zip -P ${DIR_DOWNLOAD}/
+unzip ${DIR_DOWNLOAD}/dvinstall.zip -d /home/dataverse
+cd /home/dataverse
+chown -R dataverse:dataverse /home/dataverse
+cp /home/dataverse/dvinstall/schema*.xml /usr/local/solr/solr-9.3.0/server/solr/collection1/conf
+cp /home/dataverse/dvinstall/solrconfig.xml /usr/local/solr/solr-9.3.0/server/solr/collection1/conf
+sed 's;       <jvm-options>-Xbootclasspath/a:${com.sun.aas.installRoot}/lib/grizzly-npn-api.jar</jvm-options>;       <jvm-options>-Xbootclasspath/a:${com.sun.aas.installRoot}/lib/grizzly-npn-api.jar</jvm-options>\n        <jvm-options>-Ddataverse.path.imagemagick.convert=/opt/local/bin/convert</jvm-options>;' /usr/local/payara6/glassfish/domains/domain1/config/domain.xml
+       
+# Payara
+
+useradd -m payara
+# versão 6 
+[ ! -f ${DIR_DOWNLOAD}/payara-6.2023.10.zip ] && wget https://nexus.payara.fish/repository/payara-community/fish/payara/distributions/payara/6.2023.10/payara-6.2023.10.zip -P ${DIR_DOWNLOAD}/
+unzip payara-6.2023.10.zip -d /usr/local
+#cd /home/payara
+#chown -R payara:payara /home/payara
+#mv payara6 /usr/local/.
+chown -R root:root /usr/local/payara6
+chown dataverse /usr/local/payara6/glassfish/lib
+chown -R dataverse:dataverse /usr/local/payara6/glassfish/domains/domain1
+sudo -u dataverse /usr/local/payara6/glassfish/bin/asadmin start-domain
+sudo -u dataverse /usr/local/payara6/bin/asadmin osgi lb
+# deve aparecer Command osgi executed successfully.
+r=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080)
+[ "$r"=="200" ] && echo "[INFO] Instalação do payara OK !" || echo "[ERRO] Corriga a instalação do payara !"
+sudo -u dataverse /usr/local/payara6/glassfish/bin/asadmin stop-domain
+echo '[Unit]
+Description = Payara Server
+After = syslog.target network.target
+[Service]
+User=dataverse
+Type = forking
+ExecStart = /usr/local/payara6/glassfish/bin/asadmin start-domain
+ExecStop = /usr/local/payara6/glassfish/bin/asadmin stop-domain
+ExecReload = /usr/local/payara6/glassfish/bin/asadmin restart-domain
+TimeoutSec=900
+[Install]
+WantedBy = multi-user.target
+' > /etc/systemd/system/payara.service
+systemctl daemon-reload
+systemctl enable payara.service
+systemctl restart payara.service
+systemctl status payara.service
+
+# R
+
+apt-get -y install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo apt update
+apt-get install -y r-base r-base-core r-recommended r-base-dev
+R -e 'install.packages("R2HTML", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
+R -e 'install.packages("rjson", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
+R -e 'install.packages("DescTools", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
+R -e 'install.packages("Rserve", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
+R -e 'install.packages("haven", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
+
+echo "Instalação do módulo III concluída!"
+
+echo "Instalação do módulo IV. em desenvolvimento" 
+
+# Dataverse
+
+#cd /home/dataverse/dvinstall
+
+GLASSFISH_DIRECTORY=/usr/local/payara6
+sudo -u dataverse sed -i 's;GLASSFISH_DIRECTORY.*;GLASSFISH_DIRECTORY = /usr/local/payara6;' default.config
+sudo -u dataverse sed -i 's/DOI_USERNAME =.*/DOI_USERNAME = '$DOI_USERNAME'/' default.config
+sudo -u dataverse sed -i 's/DOI_PASSWORD =.*/DOI_PASSWORD = '$DOI_PASSWORD'/' default.config
+sudo -u dataverse sed -i 's/POSTGRES_DATABASE =.*/POSTGRES_DATABASE = '$DATAVERSE_DB'/' default.config
+sudo -u dataverse sed -i 's/POSTGRES_USER =.*/POSTGRES_USER = '$DATAVERSE_DB_USER'/' default.config
+sudo -u dataverse sed -i 's/POSTGRES_PASSWORD =.*/POSTGRES_PASSWORD = '$DATAVERSE_DB_PASSWORD'/' default.config
+sudo -u dataverse sed -i 's/POSTGRES_ADMIN_PASSWORD =.*/POSTGRES_ADMIN_PASSWORD = '$POSTGRES_ADMIN_PASSWORD'/' default.config
+sudo -u dataverse sed -i 's/ADMIN_EMAIL =.*/ADMIN_EMAIL = '$ADMIN_EMAIL'/' default.config
+
+apt-get -y install python3-pip
+pip3 install psycopg2-binary #psycopg2
+python3 /home/dataverse/dvinstall/install.py -y
+
+DIR_RSERVE="/home/dataverse/dataverse-5.12.1/scripts/r/rserve/"
+#Ativando o R server no dataverse
+sed -i 's/chkconfig rserve on/update-rc.d rserve defaults/' ${DIR_RSERVE}/rserve-setup.sh
+sed -i '/^. \/etc\/rc.d\/init.d\/functions/s//#&/' ${DIR_RSERVE}/rserve-startup.sh
+
+${DIR_RSERVE}/rserve-setup.sh
+
+
+## fim
+#notas
+
+#systemctl status solr.service
+#systemctl status payara.service
+
+#multipass info $VM | grep IPv4  | cut -c 17-30
+#10.199.87.190:8080
+
+
--- install.sh
+++ install.sh
@@ -1,219 +1,148 @@
 #!/bin/bash
 
-# Instalação 
-# multipass ls
-# multipass launch --name dataverse -d 50G -m 4G -c 4
-# multipass transfer install.sh ${VM}:install.sh
-# multipass shell dataverse ou
-# multipass exec ${VM} -- sudo ./install.sh
-# multipass transfer .env ${VM}:.env
+#altere para o seu diretório de projeto
+export DIR_PROJECT="${HOME}/douglas500/sti/desenvolvimento/repositorios/dataverse"
+export DIR_PROJECT="${HOME}/douglas500/dev/dataverse/dataverse"
 
-#wget https://github.com/IQSS/dataverse/releases/download/v6.0/dvinstall.zip
-#wget https://nexus.payara.fish/repository/payara-community/fish/payara/distributions/payara/6.2023.10/payara-6.2023.10.zip
-#wget https://archive.apache.org/dist/lucene/solr/8.11.1/solr-8.11.1.tgz
-#wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
+[ -z ${DIR_PROJECT} ] && echo -e 'Informe o diretório do projeto. Ex:\n  export DIR_PROJECT="${HOME}/dev/evor"' && exit 1
 
-SOLR_VERSION="9.3.0"
+YML="${DIR_PROJECT}/dataverse/docker-compose-dev.yml"
+OPTSTRING=":umdbhi"
+BUILD=false
+FORMA="d" #d=docker ; m=multipass
 
-# DOI obtido em aula
-cd /home/ubuntu
-[ ! -f .env ] && touch .env && echo 'DOI_USERNAME="user_doi"
-DOI_PASSWORD="password_doi"
-POSTGRES_ADMIN_PASSWORD="postgres"
-DATAVERSE_DB="dataverse"
-DATAVERSE_DB_USER="dataverse"
-DATAVERSE_DB_PASSWORD="dataverse"
-ADMIN_EMAIL="email@webmail.br"
-' > .env  && echo 'Altere o arquivo .env antes de iniciar a instalação!' && exit 1
-source .env
+# Docker exemplo
+# https://github.com/docker/awesome-compose/tree/master/official-documentation-samples/django/
 
-apt-get update
-apt-get -y install libc6-i386 libc6-x32 libxi6 libxtst6 unzip wget   
-[ ! -f jdk-17_linux-x64_bin.deb ] && wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
-chown _apt /var/lib/update-notifier/package-data-downloads/partial/
-apt-get -y install ./jdk-17_linux-x64_bin.deb 
-apt-get -y install --reinstall jdk-17
+# instalando o docker na vm 
+function installApps() {
+  echo "informe a senha do usuário: "
+  sudo apt-get update -y
+  sudo apt-get install ca-certificates curl gnupg lsb-release -y
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+  # docker run hello-world 
+  sudo usermod -aG docker $USER  
+  sudo snap install multipass
+  echo "O Docker está Ok!
+Reinicie a sua VM"
+}
 
-# postgres
+#Instala em container baseado em docker-compose
+function installDocker() {
+  echo "Install via docker"
+  #Se a imagem da aplicação não existir então faça ao build
+  if [[ "$(docker images -q ${APP_IMAGE} 2> /dev/null)" == "" ]]; then
+    echo "Definindo o build da aplicação"
+    BUILD=true
+  fi
+  # se o projeto não existir, baixa:
+  if [ ! -d "${DIR_PROJECT}/dataverse" ]; then
+   echo "Projeto dataverse ainda não existe. Baixando ..."
+   git clone https://github.com/IQSS/dataverse.git ${DIR_PROJECT}/dataverse
+  fi
+  cd ${DIR_PROJECT}
+  source ${DIR_PROJECT}/dataverse/.env
+  #build do app
+  [ ${BUILD} ] && docker-compose -f ${YML} build
+  #Levanta os containers via docker em segundo plano
+  docker-compose -f ${YML} up -d
+  IP=$(hostname -I | cut -f1 -d ' ')
+  echo "Abra a página via: http://${IP}:8080"
+  cd -  
+}
 
-echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-apt-get update
-apt-get -y install postgresql-13
-pg_ctlcluster 13 main start
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*' #libera para todas as conexões/" /etc/postgresql/13/main/postgresql.conf
-#Comenta todas as linhas
-sed -i '/^#/! s/.*/#&/g' /etc/postgresql/13/main/pg_hba.conf
-#Adiciona
-sed -i '$a\
-# Dataverse \
-local   all             postgres                                trust \
-host    all             all             127.0.0.1/32            trust \
-host    all             all             ::1/128                 trust \
-host    all             all             0.0.0.0/0               trust' /etc/postgresql/13/main/pg_hba.conf
-pg_ctlcluster 13 main restart
-#service postgresql restart
-pg_ctlcluster 13 main status
-#service postgresql status
-r=$(/usr/bin/psql -At -h 127.0.0.1 -p 5432 -U postgres -d postgres -c 'SELECT 1')
-[ "$r"=="1" ] && echo "[INFO] Instalação do postgres OK !" || echo "[ERRO] Corriga a instalação do postgres !" 
+#Encerra os serviços
+function delDocker() {
+  echo "del docker"
+  cd ${DIR_PROJECT}/dataverse
+  docker-compose -f ${YML} down
+  cd -
+}
 
-# criando o usuário e db do dataverse . devem constar no .env
-#sudo -u postgres psql -c "create database $DATAVERSE_DB;"
-sudo -u postgres psql -c "create user $DATAVERSE_DB_USER with encrypted password '$DATAVERSE_DB_PASSOWRD';"
-sudo -u postgres psql -c "grant all privileges on database $DATAVERSE_DB to $DATAVERSE_DB_USER;"
+#instala em uma VM baseada no multipass
+function installMultipass() {
+  echo "Install via multipass"
+  mkdir -p ${DIR_PROJECT}/download
+  cd ${DIR_PROJECT}/download
+  #baixa os pacotes previamente
+  [ ! -f solr-9.3.0.tgz ] && wget https://archive.apache.org/dist/solr/solr/9.3.0/solr-9.3.0.tgz
+  [ ! -f jdk-17_linux-x64_bin.deb ] && wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
+  [ ! -f dvinstall.zip ] && wget https://github.com/IQSS/dataverse/releases/download/v6.0/dvinstall.zip
+  [ ! -f payara-6.2023.10.zip ] && wget https://nexus.payara.fish/repository/payara-community/fish/payara/distributions/payara/6.2023.10/payara-6.2023.10.zip
+  cd -
+  cp dataverse_install.sh download
+  cp .env download
+  VM="dataverse"
+  #multipass launch --name $VM -d 50G -m 4G -c 4  
 
-# solr
+  multipass launch --name $VM -d 20G -m 4G -c 3
+  multipass mount ${DIR_PROJECT}/download ${VM}:download
+  multipass exec ${VM} -- sudo download/dataverse_install.sh | tee log.txt  
+  IP=$(multipass info ${VM} | grep IPv4 | cut -c 17-30)
+  echo "Abra a página via: http://${IP}:8080"
+}
 
-useradd -m solr
-mkdir /usr/local/solr
-[ ! -f solr-9.3.0.tgz ] && wget https://archive.apache.org/dist/solr/solr/9.3.0/solr-9.3.0.tgz
-tar -xvzf solr-9.3.0.tgz -C /usr/local/solr
-cp -r /usr/local/solr/solr-9.3.0/server/solr/configsets/_default /usr/local/solr/solr-9.3.0/server/solr/collection1
-chown -R solr:solr /usr/local/solr
-cd /usr/local/solr
+#Exclui as vms e os dados gerados
+function delMultipass() {
+  echo "del multipass"
+  VM="dataverse"
+  multipass stop ${VM} && multipass delete ${VM} && multipass purge
+}
 
-sed -i 's;<Set name="requestHeaderSize"><Property name="solr.jetty.request.header.size" default="8192" /></Set>;<Set name="requestHeaderSize"><Property name="solr.jetty.request.header.size" default="102400" /></Set>;' /usr/local/solr/solr-9.3.0/server/etc/jetty.xml
-sed -i '$a#Instalação dataverse' /etc/security/limits.conf
-sed -i '$asolr soft nproc  65000' /etc/security/limits.conf
-sed -i '$asolr hard nproc  65000' /etc/security/limits.conf
-sed -i '$asolr soft nofile 65000' /etc/security/limits.conf
-sed -i '$asolr hard nofile 65000' /etc/security/limits.conf 
-sed -i '$aroot soft nproc  65000' /etc/security/limits.conf 
-sed -i '$aroot hard nproc  65000' /etc/security/limits.conf 
-sed -i '$aroot soft nofile 65000' /etc/security/limits.conf 
-sed -i '$aroot hard nofile 65000' /etc/security/limits.conf
-cd /usr/local/solr/solr-9.3.0
-sudo -u solr bin/solr start
-sudo -u solr bin/solr create_core -c collection1 -d server/solr/collection1/conf/
+function usage {
+  echo "./$(basename $0) [${OPTSTRING}]"
+  echo "  -u: up dataverse"
+  echo "  -d: down dataverse"
+  echo "  -m: instala via multipas e não via docker"
+  echo "  -b: executa o build"
+  echo "  -i: Instala o docker"
+  echo "  -h: help"  
+}
 
-echo '[Unit]
-Description = Apache Solr
-After = syslog.target network.target remote-fs.target nss-lookup.target
-[Service]
-User = solr
-Type = forking
-WorkingDirectory = /usr/local/solr/solr-9.3.0
-ExecStart = bash /usr/local/solr/solr-9.3.0/bin/solr start
-ExecStop = bash /usr/local/solr/solr-9.3.0/bin/solr stop
-ExecReload= bash /usr/local/solr/solr-9.3.0/bin/solr stop
-LimitNOFILE=65000
-LimitNPROC=65000
-Restart=on-failure
-[Install]
-WantedBy = multi-user.target
-' > /etc/systemd/system/solr.service
-sudo -u solr bin/solr stop
-systemctl daemon-reload
-systemctl restart solr.service
-systemctl enable solr.service
-systemctl status solr.service
+ACTION=""
+SILENT=""
+RMVOLUME=""
 
-# Dataverse
+while getopts ${OPTSTRING} ARG; do
+  case ${ARG} in
+    m)
+      FORMA="m"
+      ;;
+    u)
+      ACTION="up" 
+      ;;
+    d)
+      ACTION="down" 
+      ;;
+    b) 
+      BUILD=true
+      ;;
+    i) 
+      installApps
+      exit 0
+      ;;
+    h | *) 
+      usage
+      exit 1
+      ;;
+  esac
+done
 
-apt-get -y install jq imagemagick curl libssl-dev libcurl4-openssl-dev
-useradd -m dataverse
-cd /home/ubuntu
-[ ! -f dvinstall.zip ] && wget https://github.com/IQSS/dataverse/releases/download/v6.0/dvinstall.zip
-unzip dvinstall.zip -d /home/dataverse
-cd /home/dataverse
-chown -R dataverse:dataverse /home/dataverse
-cp /home/dataverse/dvinstall/schema*.xml /usr/local/solr/solr-9.3.0/server/solr/collection1/conf
-cp /home/dataverse/dvinstall/solrconfig.xml /usr/local/solr/solr-9.3.0/server/solr/collection1/conf
-sed 's;       <jvm-options>-Xbootclasspath/a:${com.sun.aas.installRoot}/lib/grizzly-npn-api.jar</jvm-options>;       <jvm-options>-Xbootclasspath/a:${com.sun.aas.installRoot}/lib/grizzly-npn-api.jar</jvm-options>\n        <jvm-options>-Ddataverse.path.imagemagick.convert=/opt/local/bin/convert</jvm-options>;' /usr/local/payara6/glassfish/domains/domain1/config/domain.xml
-       
-# Payara
-
-useradd -m payara
-cd /home/ubuntu
-# versão 6 
-[ ! -f payara-6.2023.10.zip ] && wget https://nexus.payara.fish/repository/payara-community/fish/payara/distributions/payara/6.2023.10/payara-6.2023.10.zip
-unzip payara-6.2023.10.zip -d /usr/local
-#cd /home/payara
-#chown -R payara:payara /home/payara
-#mv payara6 /usr/local/.
-chown -R root:root /usr/local/payara6
-chown dataverse /usr/local/payara6/glassfish/lib
-chown -R dataverse:dataverse /usr/local/payara6/glassfish/domains/domain1
-sudo -u dataverse /usr/local/payara6/glassfish/bin/asadmin start-domain
-sudo -u dataverse /usr/local/payara6/bin/asadmin osgi lb
-# deve aparecer Command osgi executed successfully.
-r=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8080)
-[ "$r"=="200" ] && echo "[INFO] Instalação do payara OK !" || echo "[ERRO] Corriga a instalação do payara !"
-sudo -u dataverse /usr/local/payara6/glassfish/bin/asadmin stop-domain
-echo '[Unit]
-Description = Payara Server
-After = syslog.target network.target
-[Service]
-User=dataverse
-Type = forking
-ExecStart = /usr/local/payara6/glassfish/bin/asadmin start-domain
-ExecStop = /usr/local/payara6/glassfish/bin/asadmin stop-domain
-ExecReload = /usr/local/payara6/glassfish/bin/asadmin restart-domain
-TimeoutSec=900
-[Install]
-WantedBy = multi-user.target
-' > /etc/systemd/system/payara.service
-systemctl daemon-reload
-systemctl enable payara.service
-systemctl restart payara.service
-systemctl status payara.service
-
-# R
-
-apt-get -y install --no-install-recommends software-properties-common dirmngr
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-sudo apt update
-apt-get install -y r-base r-base-core r-recommended r-base-dev
-R -e 'install.packages("R2HTML", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
-R -e 'install.packages("rjson", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
-R -e 'install.packages("DescTools", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
-R -e 'install.packages("Rserve", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
-R -e 'install.packages("haven", repos="https://cloud.r-project.org/", lib="/usr/lib/R/library" )'
-
-echo "Instalação do módulo III concluída!"
-
-echo "Instalação do módulo IV. em desenvolvimento" 
-
-# Dataverse
-
-cd /home/dataverse/dvinstall
-
-GLASSFISH_DIRECTORY=/usr/local/payara6
-sudo -u dataverse sed -i 's;GLASSFISH_DIRECTORY.*;GLASSFISH_DIRECTORY = /usr/local/payara6;' default.config
-sudo -u dataverse sed -i 's/DOI_USERNAME =.*/DOI_USERNAME = '$DOI_USERNAME'/' default.config
-sudo -u dataverse sed -i 's/DOI_PASSWORD =.*/DOI_PASSWORD = '$DOI_PASSWORD'/' default.config
-sudo -u dataverse sed -i 's/POSTGRES_DATABASE =.*/POSTGRES_DATABASE = '$DATAVERSE_DB'/' default.config
-sudo -u dataverse sed -i 's/POSTGRES_USER =.*/POSTGRES_USER = '$DATAVERSE_DB_USER'/' default.config
-sudo -u dataverse sed -i 's/POSTGRES_PASSWORD =.*/POSTGRES_PASSWORD = '$DATAVERSE_DB_PASSWORD'/' default.config
-sudo -u dataverse sed -i 's/POSTGRES_ADMIN_PASSWORD =.*/POSTGRES_ADMIN_PASSWORD = '$POSTGRES_ADMIN_PASSWORD'/' default.config
-sudo -u dataverse sed -i 's/ADMIN_EMAIL =.*/ADMIN_EMAIL = '$ADMIN_EMAIL'/' default.config
-
-apt-get -y install python3-pip
-pip3 install psycopg2-binary #psycopg2
-python3 install.py -y
-
-cd /home/dataverse/dataverse-5.12.1/scripts/r/rserve/
-#Ativando o R server no dataverse
-sed -i 's/chkconfig rserve on/update-rc.d rserve defaults/' rserve-setup.sh
-sed -i '/^. \/etc\/rc.d\/init.d\/functions/s//#&/' rserve-startup.sh
-
-./rserve-setup.sh
-
-
-
-
-
-
-
-## fim
-#notas
-
-#systemctl status solr.service
-#systemctl status payara.service
-
-#multipass info $VM | grep IPv4  | cut -c 17-30
-#10.199.87.190:8080
-
+case ${ACTION} in
+  "up")
+    [ "$FORMA" == "d" ] && installDocker || installMultipass 
+    ;;
+  "down")
+    [ "$FORMA" == "d" ] && delDocker || delMultipass 
+    ;;
+  *) 
+    usage
+    exit 1
+    ;;
+esac
 
